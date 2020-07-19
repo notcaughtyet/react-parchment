f9c4e5e49a6f43d42ceb83dc834ef9cf7ac5c2a9
metacryst <samrussell99@gmail.com>
2020-07-18 19:08:07 -0500

    double click focus fix and rudimentary word spawn

--- src/App.js
+++ src/App.js
@@ -4,31 +4,47 @@ import './App.css';
 
 function App() {
   
+    const [textBox1, setTextBox1] = useState(<div className="testDiv" style={{display: "hidden"}} ></div>)
+    
+    const [text1, setText1] = useState(<p className="text1"></p>)
+    
     useEffect(() => {
       console.log('useffecting')
     })
   
-    const [textBox1, setTextBox1] = useState(<div className="testDiv" style={{display: "hidden"}} ></div>)
+    
   
     document.addEventListener('mouseup', checkForSelection);
   
     
     // let textBox = <div className="testDiv" style={{display: "hidden"}} ></div>
-
+    let posX
+    let posY
     
     function newText(e) {
       e.preventDefault()
       
-      var posX = e.clientX;
-      var posY = e.clientY;
+      posX = e.clientX;
+      posY = e.clientY;
       
       console.log('double click')
       console.log(posX, posY)
       
-      setTextBox1(<input className="mainInput" autoFocus="autofocus" onFocus={handleFocus} style={{left: posX - 3, top: posY - 11}}></input>)
+      setTextBox1(<input className="mainInput" autoFocus="autofocus" onFocus={handleFocus} onMouseOver={handleFocus} onKeyUp={handleEnter} style={{left: posX - 3, top: posY - 11}}></input>)
+            
     }
     
-    
+    function handleEnter(e) {
+        if (e.keyCode === 13) {
+          console.log(posX);
+          console.log(e.target.value)
+          
+          setText1(<p className="text1">{e.target.value}</p>)
+          
+          e.target.value = ""
+        }
+      }
+      
     const handleFocus = (event) => event.target.select();
     
     
@@ -95,18 +111,22 @@ function App() {
     
 
     
+    
+    
   return (
     <div className="App" onDoubleClick={newText}>
       {textBox1}
-      <div>hello</div>
-      <p>h</p>
-      <p>e</p>
-      <p>llo</p>
+      {text1}
     </div>
   );
 }
 
 export default App;
+
+{/* <div>hello</div>
+<p>h</p>
+<p>e</p>
+<p>llo</p> */}
 
 
 // const divs = []
